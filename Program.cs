4f2792609e7a7cb7ebbab6ac2b853564feb41a82
sipra <sipramishra085@gmail.com>
2023-04-04 15:18:37 +0530

    check last name valid or not

--- Program.cs
+++ Program.cs
@@ -8,12 +8,18 @@ namespace UserRegistrationProblem
         {
             Console.WriteLine("Welcome to User Registration Page!!");
             Console.WriteLine("********************************");
-            UserRegistationCode obj = new UserRegistationCode();
+            UserRegistrationCode obj = new UserRegistrationCode();
             Console.WriteLine("Enter the First Name:");
             string FName = Console.ReadLine();
-           
+            Console.WriteLine("Enter the Last Name:");
+            string LName = Console.ReadLine();
+
+            Console.WriteLine(obj.ValidateFName(FName));
+            Console.WriteLine(obj.ValidateFName(LName));
         }
+
     }
+    
 }
 
 
