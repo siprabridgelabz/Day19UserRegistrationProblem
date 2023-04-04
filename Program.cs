f45af7aa30db6660ac4172c793218a7f655ff1be
sipra <sipramishra085@gmail.com>
2023-04-04 15:09:31 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Page!!");
+            Console.WriteLine("********************************");
+            UserRegistationCode obj = new UserRegistationCode();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+           
+        }
+    }
+}
+
+
