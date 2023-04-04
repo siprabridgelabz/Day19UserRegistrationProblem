df70b59f0681c00976c1a4cb98c202624615187e
sipra <sipramishra085@gmail.com>
2023-04-04 15:14:16 +0530

    check first name valid or not

---
+++ UserRegistationCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public  class UserRegistationCode
+    {
+         
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+       
+
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+        
+    }
+}
